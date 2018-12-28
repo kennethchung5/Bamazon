1129574035710cdd655dc7a2a5dc9e2099154f39
Kenneth Chung <kennethchung5@gmail.com>
2018-12-28 03:54:00 -0500

    finished Challenge #1: Customer View

--- bamazon.sql
+++ bamazon.sql
@@ -2,6 +2,10 @@
 CREATE DATABASE bamazon;
 
 use bamazon;
+*/
+
+DROP TABLE products;
+
 
 CREATE TABLE products (
 	item_id INT NOT NULL AUTO_INCREMENT,
@@ -10,10 +14,20 @@ CREATE TABLE products (
     price DECIMAL(9,2) NOT NULL,
     stock_quantity INT NOT NULL,
     primary key(item_id)
-)
-*/
+);
 
-SELECT * FROM products;
+
+
+
+
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ()
+VALUES 
+("laptop", "computers", 799.99, 4),
+("digital piano", "musical instruments", 600.00, 3),
+("cooking utensils", "home goods", 20.99, 8),
+("wallet", "accessories", 15.50, 6),
+("optical mouse", "computers", 14.99, 15)
+;
+
+SELECT * FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
+require("console.table");
+
 var connection = mysql.createConnection({
   host: "localhost",  
   port: 3306,
@@ -15,20 +17,88 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("Connection established");
+  console.log("Welcome to Bamazon!");
 
-  presentItems();
-  // connection.end();
+  setTimeout(presentItems, 2000);  
 });
 
 
+function returnMainMenu() {
+  console.log("Returning to main menu...");
+  setTimeout(presentItems, 2000);
+}
+
 function presentItems() {
+
   connection.query("SELECT item_id, product_name, price FROM products ORDER BY item_id", function(error, results) {
     if (error) throw error;
 
-    console.log(results);
+    console.log("\n");
+    console.table(results);
 
-
-
+    takeOrder();
   })
 }
+
+function takeOrder() {
+  inquirer.prompt([
+    {
+      name: "itemChoice",
+      message: "Enter the item_id of the product you wish to purchase.",
+      type: "input",
+      validate: function(input) {
+          if (input == parseInt(input)) {
+            return true;
+          } else {
+            return "Please enter an integer."
+          }
+        }
+    },
+    {
+      name: "quantity",
+      message: "Enter a quantity (in units) to purchase.",
+      type: "input",
+      validate: function(input) {
+        if (input == parseInt(input)) {
+          return true;
+        } else {
+          return "Please enter an integer."
+        }
+      }
+    }
+  ]).then(function(answers) {
+
+    connection.query("SELECT stock_quantity, price FROM products WHERE ?", 
+                      [{item_id: answers.itemChoice}], 
+                      function(error, results) {
+                        if (error) throw error;
+                        if (results.length === 0) {
+                          console.log("There is no item with that id.");
+
+                          returnMainMenu();
+                        } else if (results[0].stock_quantity >= answers.quantity) {
+                          // transaction goes through; calculate new stock and transaction cost first
+                          var newStock = results[0].stock_quantity - answers.quantity;
+                          var transactionCost = (answers.quantity * results[0].price).toFixed(2);
+                          
+                          connection.query("UPDATE products SET ? WHERE ?", 
+                          [{stock_quantity: newStock}, 
+                          {item_id: answers.itemChoice}], 
+                          function(error) {
+                            if (error) throw error;
+
+                            console.log("Transaction confirmed! Your total is $" + transactionCost);
+
+                            returnMainMenu();
+                          })
+                        } else {
+                          //insufficient quantity
+                          console.log("Sorry, we don't have enough of this item to fulfill your request.");
+
+                          returnMainMenu();
+                        }
+                    
+                      })
+
+  })
+};
